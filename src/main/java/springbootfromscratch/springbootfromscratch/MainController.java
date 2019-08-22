8a6e04abdcdc01771baefc90d9bae3d3cf1adc56
Xanenon <philipp.krenitz@gmx.de>
2019-08-22 15:08:21 +0200

    Test from GitHub itself

--- src/main/java/springbootfromscratch/springbootfromscratch/MainController.java
+++ src/main/java/springbootfromscratch/springbootfromscratch/MainController.java
@@ -27,7 +27,6 @@ public class MainController {
         return userRepository.findByNachname(nachname);
     }
 
-    // fgsdfg
     /*
     @GetMapping(value = "/id/{id}")
     public @ResponseBody Optional<User> getUserById(@PathVariable(value = "id") Integer id) {
