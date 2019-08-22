1523f4b6833fb6b9a56d20cb97ff286726eb127a
Xanenon <philipp.krenitz@gmx.de>
2019-08-22 14:23:51 +0200

    push with pom.xml in mainfolder

---
+++ src/test/java/springbootfromscratch/springbootfromscratch/SpringbootfromscratchApplicationTests.java
@@ -0,0 +1,16 @@
+package springbootfromscratch.springbootfromscratch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootfromscratchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
